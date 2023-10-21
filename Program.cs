c059f8e4f080f5dbcc644592a715fd754f4f808e
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-10-21 16:47:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace Lesson_20_10
+{
+    internal class Program
+    {
+        public class Joke
+        {
+            public string joke { get; set; }
+        }
+
+        static async Task Main()
+        {
+            string url = "https://geek-jokes.sameerkumar.website/api?format=json";
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                string joke_txt = await response.Content.ReadAsStringAsync();
+
+                Joke joke = JsonConvert.DeserializeObject<Joke>(joke_txt);
+
+                string[] sentences = Regex.Split(joke.joke, @"(?<=[.!?])\s+");
+                foreach (string sentence in sentences)
+                {
+                    Console.WriteLine(sentence);
+                }
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
